8d9a442bae5f0c01c2093eb13013229904016507
aashiralinazmi <103604566+aashiralinazmi@users.noreply.github.com>
2022-04-14 02:25:45 +0500

    initial commit

---
+++ Form3.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CARE_RACE_GAME
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.EASY = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // EASY
+            // 
+            this.EASY.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EASY.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.EASY.Location = new System.Drawing.Point(99, 91);
+            this.EASY.Name = "EASY";
+            this.EASY.Size = new System.Drawing.Size(75, 23);
+            this.EASY.TabIndex = 0;
+            this.EASY.Text = "EASY";
+            this.EASY.UseVisualStyleBackColor = true;
+            this.EASY.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.EASY);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Location = new System.Drawing.Point(15, 0);
+            this.Name = "Form2";
+            this.Text = "LEVELS";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EASY;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ PlayerInfo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CARE_RACE_GAME
+{
+    public partial class PlayerInfo : Form
+    {
+        public PlayerInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayerInfo_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = "A";
+            FileStream fs = new FileStream("d:\\playerinfo.txt",
+            FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            while (str != "")
+            {
+                
+                sw.WriteLine(str);
+                
+            }
+            sw.Close();
+            fs.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("d:\\playerinfo.txt",
+         FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string input = Console.ReadLine();
+            string str = sr.ReadLine();
+            while (str != null)
+            {
+                if (str.Contains(input))
+                {
+                    Console.WriteLine("This player's highscore is " + str);
+                }
+                
+            }
+            
+            sr.Close();
+            fs.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("d:\\playerinfo.txt",
+        FileMode.Open, FileAccess.Read);
+            
+            
+            
+            fs.Close();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
